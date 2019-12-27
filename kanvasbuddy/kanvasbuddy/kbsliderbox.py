21a43470b73ef5b0ec8c69f3a9be7cb295c6bfde
Kapyia <kapyia.art@gmail.com>
2019-12-27 13:44:32 +0100

    Implemented new Slider/Spinboxes
    They actually seem to be working!

--- kanvasbuddy/kanvasbuddy/kbsliderbox.py
+++ kanvasbuddy/kanvasbuddy/kbsliderbox.py
@@ -13,67 +13,132 @@
 # You should have received a copy of the GNU General Public License
 # along with KanvasBuddy. If not, see <https://www.gnu.org/licenses/>.
 
-from PyQt5.QtWidgets import QWidget, QSlider, QSpinBox, QGridLayout
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QStackedLayout, QProgressBar, QSpinBox
 from PyQt5.QtCore import Qt
 
-class KBSlider(object):
+class KBSliderSpinBox(QWidget):
 
-    def __init__(self, min, max):
-        self.qslider = QSlider(Qt.Horizontal)
-        self.qslider.setMinimumWidth(100)
+    def __init__(self, min = 0, max = 100, parent=None):
+        super(KBSliderSpinBox, self).__init__(parent)
+        self.setMinimumWidth(100)
+        self.setFixedHeight(16)
+        self.setLayout(QStackedLayout(self))
+        self.layout().setStackingMode(QStackedLayout.StackAll)
+        self.setCursor(Qt.SplitHCursor)
+        self.scaling = 1
 
-        self.qspinbox = QSpinBox()
-        self.qspinbox.setAlignment(Qt.AlignHCenter)
-        self.qspinbox.setButtonSymbols(2)
-        font = self.qspinbox.font()
-        font.setPixelSize(9)
-        self.qspinbox.setFont(font)
-        self.qspinbox.setFixedHeight(20)
+        self.progbar = QProgressBar(self)
+        self.progbar.setRange(min, max)
+        self.progbar.setValue(0)
 
-        self.setRange(min, max)
+        self.spinbox = QSpinBox(self)
+        self.spinbox.setRange(min, max)
+        self.spinbox.setAlignment(Qt.AlignHCenter)
+        self.spinbox.setButtonSymbols(self.spinbox.NoButtons)
 
-        self.qslider.valueChanged.connect(self.qspinbox.setValue)
-        self.qspinbox.valueChanged.connect(self.qslider.setValue)
+        self.layout().addWidget(self.spinbox)
+        self.layout().addWidget(self.progbar)
 
-    def setPrefix(self, prefix):
-        self.qspinbox.setPrefix(prefix)
+        self.spinbox.hide()
 
-    def setSuffix(self, suffix):
-        self.qspinbox.setSuffix(suffix)
+        self.spinbox.editingFinished.connect(self.closeSpinBox)
+        self.spinbox.lineEdit().returnPressed.connect(self.closeSpinBox)
+        self.spinbox.valueChanged.connect(self.updateFormat)
+        self.spinbox.valueChanged.connect(self.updateProgBar)
+
+    def mousePressEvent(self, e):
+        if e.buttons() == Qt.LeftButton:
+            self.setFocus()
+            delta = e.pos().x() / self.width()
+            self.spinbox.setValue(delta**self.scaling * self.spinbox.maximum())
+            self.progbar.setValue(delta * self.progbar.maximum())
+
+    def updateProgBar(self):
+        delta = (self.spinbox.value() / self.spinbox.maximum())**(1./self.scaling)
+        self.progbar.setValue(delta * self.progbar.maximum())
     
-    def setAffixes(self, prefix, suffix):
-        self.qspinbox.setPrefix(prefix)
-        self.qspinbox.setSuffix(suffix)
+    def mouseMoveEvent(self, e):
+        self.mousePressEvent(e)
+    
+    def mouseDoubleClickEvent(self, e):
+        self.openSpinBox()
 
+    def keyPressEvent(self, e):
+        if (e.key() == Qt.Key_Return or
+            e.key() == Qt.Key_Enter or
+            e.key() == Qt.Key_Escape):
+            if self.spinbox.isVisible():
+                self.closeSpinBox()
+        elif e.text().isdecimal():
+            if self.spinbox.isHidden():
+                self.openSpinBox()
+                self.spinbox.lineEdit().keyPressEvent(e)
+
+    def closeSpinBox(self):
+        self.spinbox.clearFocus()
+        self.spinbox.hide()
+
+    def openSpinBox(self):
+        self.spinbox.show()
+        self.spinbox.lineEdit().clear()
+        self.spinbox.lineEdit().setFocus()
+    
+    def setMinimum(self, min):
+        self.spinbox.setMinimum(min)
+
+    def setMaximum(self, max):
+        self.spinbox.setMinimum(max)
+    
     def setRange(self, min, max):
-        self.qslider.setRange(min, max)
-        self.qspinbox.setRange(min, max)
-    
-    def setValue(self, value):
-        self.qslider.setValue(value)
+        self.spinbox.setRange(min, max)
+
+    def setScaling(self, s):
+        self.scaling = s
+
+    def updateFormat(self):
+        self.progbar.setFormat(f"{self.spinbox.prefix()}{self.spinbox.value()}{self.spinbox.suffix()}")
     
     def value(self):
-        return self.qslider.value()
+        return self.spinbox.value()
+    
+    def setValue(self, val):
+        self.spinbox.setValue(val)
+        self.updateProgBar()
+
+    def setExponential(self, ex):
+        if isinstance(ex, bool):
+            self.exponential = ex
+        else:
+            raise TypeError("invalid argument; active state must be a boolean")
+        
+    def setPrefix(self, pre):
+        self.spinbox.setPrefix(pre)
+
+    def setSuffix(self, suf):
+        self.spinbox.setSuffix(suf)
+    
+    def setAffixes(self, pre, suf):
+        self.spinbox.setPrefix(pre)
+        self.spinbox.setSuffix(suf)
+        self.updateFormat()
 
     def connectValueChanged(self, func):
-        self.qslider.valueChanged.connect(func)
-        self.qspinbox.valueChanged.connect(func)
+        self.spinbox.valueChanged.connect(func)
+
 
 class KBSliderBox(QWidget):
 
     def __init__(self, parent):
         super(KBSliderBox, self).__init__(parent)
-        self.setLayout(QGridLayout())
+        self.setLayout(QVBoxLayout())
 
-        self.layout().setContentsMargins(0, 0, 0, 0)
-        self.layout().setVerticalSpacing(2)
+        self.layout().setContentsMargins(2, 0, 2, 0)
+        self.layout().setSpacing(4)
         self.sliders = {}
 
     def addSlider(self, name, min, max):
-        self.sliders[name] = KBSlider(min, max)
-        newRow = self.layout().rowCount()
-        self.layout().addWidget(self.sliders[name].qslider, newRow, 0)
-        self.layout().addWidget(self.sliders[name].qspinbox, newRow, 1)
+        self.sliders[name] = KBSliderSpinBox(min, max)
+        self.layout().addWidget(self.sliders[name])
 
     def slider(self, name):
         return self.sliders[name]
